eec07af2d8061792656d9c430cbee7f993698e44
Jose Luis <joseluis_jhl@outlook.com>
2025-09-16 00:55:23 -0400

    Add solution for find-closest-number-to-zero

---
+++ leetcode/find-closest-number-to-zero.py
@@ -0,0 +1,17 @@
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        
+        int closestNum = nums[0];
+
+        for(int x: nums){
+            int ax = Math.abs(x);
+            int ac = Math.abs(closestNum);
+
+           if(ax < ac || (ax == ac && x > closestNum)) { 
+            closestNum = x;
+        }
+        }
+        
+        return closestNum;
+    }
+}
