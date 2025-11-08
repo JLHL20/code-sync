9763b818c38992686c5e5245c2cda4975398806d
Jose Luis <joseluis_jhl@outlook.com>
2025-11-07 21:49:19 -0500

    Update solution for is-subsequence

--- leetcode/is-subsequence.py
+++ leetcode/is-subsequence.py
@@ -5,13 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if s == '': return True
+        if s == '': return True 
         if len(s) > len(t): return False
-        A = 0
-        B = 0
+        A = 0 #First Pointer
+        B = 0 #Second Pointer
 
+        #Loop through the String
         while A < len(s) and B < len(t):
+            #Checks if they are equal
             if s[A] == t[B]:
-                A += 1
-            B += 1
-        return A == len(s)
+                A += 1 #Updates First Pointer
+            B += 1 #Always Updates Second Pointer
+        return A == len(s) #If the First pointer ever gets to the same length means we finish
+        #return True, otherwise false
