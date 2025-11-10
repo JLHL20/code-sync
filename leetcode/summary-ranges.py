05327fd8025c6cdab57bab2a99e9e378caaee40c
Jose Luis <joseluis_jhl@outlook.com>
2025-11-10 18:49:27 -0500

    Update solution for summary-ranges

--- leetcode/summary-ranges.py
+++ leetcode/summary-ranges.py
@@ -1,22 +1,35 @@
-class Solution(object):
-    def summaryRanges(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[str]
-        """
-        ans = []     
-        i = 0 
-        
-        while i < len(nums): 
-            start = nums[i]  
-            while i < len(nums)-1 and nums[i] + 1 == nums[i + 1]: 
-                i += 1 
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        //Create a temp List for
+        List<String> ans = new ArrayList<>();
+
+        //Length
+        int len = nums.length;
+        int i = 0;
+
+        while(i < len){
+            //Save the point on the  nums[i]
+            int StartPoint = nums[i];
             
-            if start != nums[i]: 
-                ans.append(str(start) + "->" + str(nums[i]))
-            else: 
-                ans.append(str(nums[i]))
-            
-            i += 1
- 
-        return ans
+            //loop to increment i while is a sum of 1
+            while(i < len - 1 && nums[i] + 1 == nums[i + 1]){
+                i++;
+            }
+
+            //Check if start point is not equal to the inital value after the increment
+            if(StartPoint != nums[i]){
+                ans.add(StartPoint + "->" + nums[i]);
+            }
+            //added them anyways
+            else{
+                ans.add(String.valueOf(nums[i]));
+            }
+            i++;
+        }
+        return ans;
+
+    }
+}
